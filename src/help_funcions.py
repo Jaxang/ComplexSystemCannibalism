1ba7a207a7730175fb7ada145257ee93e6d07b9a
zewinx <75088438+zewinx@users.noreply.github.com>
2020-12-04 14:33:37 +0100

    Update help_funcions.py

--- src/help_funcions.py
+++ src/help_funcions.py
@@ -5,7 +5,7 @@ from cannabalist import Cannabalist
 def get_ind_population(population):
     population_size = length(population)
     nr_can = population.count(Cannabalist) 
-    nr_non = population_size - nr_can
-    counter = [nr,can, nr_non]
-    return counter
+    nr_non = population_size - nr_can # None-cannabalist
+    
+    return nr_can, nr_non
         
