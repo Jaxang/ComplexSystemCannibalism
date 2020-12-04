f70b1074e17ed567afef86c50105549f977a5d12
Samicus <samneme55@gmail.com>
2020-12-04 11:47:11 +0100

    small changes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from cannibalist import Cannibalist
 from regular import Regular
 import graphs
 import random
-
+import numpy as np
+         
 
 
 if __name__ == '__main__':
@@ -26,10 +27,23 @@ if __name__ == '__main__':
 
     # main code 
     for i in range(nr_time_steps):
+        temp_population = np
         random.shuffle(population)
         if len(population) % 2 == 0: # if population is even
             for j  in range(0,len(population), 2):
-                tournament(population[j], population[j+1]
+                return_code = population[j].tournament(population[j+1]) # 0 both survive, 1 - other dies, 2 self dies 3 - reproduce
+                if return_code == 1:
+                    population.remove(j+1)
+                if return_code == 2:
+                    population.remove(j)
+                if return_code == 3:
+                    if type(population[j]) == Cannibalist:
+                        cannibal = Cannibalist(0.1)
+                        population.append(cannibal)
+                    if type(population[j]) == Regular:
+                        regular = Regular(0.1)
+                        population.append(regular)
+
 
         else:   # is uneven
             for j in range(0,len(population) - 1, 2):
@@ -37,4 +51,3 @@ if __name__ == '__main__':
         
             
 
-    
